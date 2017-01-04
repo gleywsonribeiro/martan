63f5d00e67a8431027a1efb33a5bad832ea54d05
Gleywson <Gleywson@hmue00118605>
2017-01-04 19:27:07 -0300

    Modelagem do Pedido. a parte mais complexa será aqui.

---
+++ src/main/java/br/com/icone/martan/modelo/Pedido.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.icone.martan.modelo;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Gleywson
+ */
+@Entity
+public class Pedido implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataCriacao;
+    
+    private String observacao;
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataEntrega;
+    
+    private BigDecimal valorFrete = BigDecimal.ZERO;
+    private BigDecimal valorDesconto = BigDecimal.ZERO;
+    private BigDecimal valorTotal = BigDecimal.ZERO;
+    private StatusPedido status = StatusPedido.ORCAMENTO;
+    private FormaPagamento formaPagamento;
+    private Usuario vendedor;
+    private Cliente cliente;
+    
+//    private EnderecoEntrega enderecoEntrega;
+//    private List<ItemPedido> itens = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pedido)) {
+            return false;
+        }
+        Pedido other = (Pedido) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.icone.martan.modelo.Pedido[ id=" + id + " ]";
+    }
+
+}
