ce9826fbfda150b5d5b0e51317452568f30e0f5b
raque <raque@192.168.1.9>
2017-01-09 23:08:50 -0300

    Desenvolvendo a tela de pedido

---
+++ src/main/java/br/com/icone/martan/controle/PedidoController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.icone.martan.controle;
+
+import br.com.icone.martan.modelo.Pedido;
+import br.com.icone.martan.modelo.repositorio.PedidoFacade;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.Date;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author raque
+ */
+@Named(value = "pedidoController")
+@SessionScoped
+public class PedidoController implements Serializable {
+
+    @Inject
+    private LoginController lc;
+    
+    @Inject
+    private PedidoFacade repositorio;
+    
+    private Pedido pedido;
+
+    public PedidoController() {
+        this.pedido = new Pedido();
+        this.pedido.setDataCriacao(new Date());
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+    
+    
+    
+    
+    
+}
