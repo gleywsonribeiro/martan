cfb7d60bf562a05ca7acd936a88be91e45f6954e
Gleywson <Gleywson@dell-gleywson.Home>
2017-04-23 12:03:07 -0300

    Finalmente a tela de venda esta terminada. Agora trabalhar no financeiro

--- src/main/java/br/com/icone/martan/controle/VendaController.java
+++ src/main/java/br/com/icone/martan/controle/VendaController.java
@@ -5,12 +5,15 @@
  */
 package br.com.icone.martan.controle;
 
+import br.com.icone.martan.modelo.FormaPagamento;
 import br.com.icone.martan.modelo.ItemPedido;
 import br.com.icone.martan.modelo.Pedido;
 import br.com.icone.martan.modelo.Produto;
 import br.com.icone.martan.modelo.TipoPedido;
+import br.com.icone.martan.modelo.Usuario;
 import br.com.icone.martan.modelo.repositorio.PedidoFacade;
 import br.com.icone.martan.modelo.repositorio.ProdutoFacade;
+import br.com.icone.martan.modelo.repositorio.UsuarioFacade;
 import br.com.icone.martan.util.jsf.JsfUtil;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
@@ -29,34 +32,40 @@ public class VendaController implements Serializable {
 
     private Pedido venda;
     private ItemPedido item;
-    
+
     @Inject
     private PedidoFacade repositorio;
-    
+
     @Inject
     private ProdutoFacade produtoRepository;
-    
+
+    @Inject
+    private UsuarioFacade usuarioRepository;
+
     public VendaController() {
+        novaVenda();
+    }
+    
+    private void novaVenda() {
         venda = new Pedido();
         venda.setTipo(TipoPedido.VENDA);
         item = new ItemPedido();
     }
 
     public void salvar() {
-        if(venda.isNovo()) {
+        if (venda.isNovo()) {
             repositorio.create(venda);
-        }
-        else {
+        } else {
             repositorio.edit(venda);
         }
         JsfUtil.addMessage("Venda salva!");
+        novaVenda();
     }
-    
+
     public Pedido getVenda() {
         return venda;
     }
 
-    
     public void setVenda(Pedido venda) {
         this.venda = venda;
     }
@@ -68,9 +77,54 @@ public class VendaController implements Serializable {
     public void setItem(ItemPedido item) {
         this.item = item;
     }
-    
+
     public List<Produto> buscaProdutoCodigo(String cod) {
         return produtoRepository.getProdutosPorCodigo(cod);
     }
-    
+
+    public List<Usuario> getVendedores() {
+        return usuarioRepository.findAll();
+    }
+
+    public FormaPagamento[] getFormasPagamento() {
+        return FormaPagamento.values();
+    }
+
+    public void recalcularPedido() {
+        this.venda.recalcularValorTotal();
+    }
+
+    public List<Produto> buscaProdutoDescricao(String descricao) {
+        return produtoRepository.getProdutosPorDescricao(descricao);
+    }
+
+    public void removerItem() {
+        venda.getItens().remove(item);
+        this.item = new ItemPedido();
+        this.venda.recalcularValorTotal();
+    }
+
+    public void adicionar() {
+        if (venda.isJaExiste(item)) {
+            //Já adiciona logo pra não ter que percorrer a lista de novo para ajustar a quantidade
+        } else {
+            this.item.setPedido(venda);
+            this.venda.getItens().add(item);
+        }
+
+        item = new ItemPedido();
+        this.venda.recalcularValorTotal();
+    }
+
+    public boolean isAdicionarAtivo() {
+        return isNaoTemItem();
+    }
+
+    public boolean isTemItem() {
+        return this.item.getProduto() != null;
+    }
+
+    public boolean isNaoTemItem() {
+        return !isTemItem();
+    }
 }
